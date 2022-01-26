5b5eddbfffa22632b705494ba2ae3823f722690c
gottmi1 <91591041+gottmi1@users.noreply.github.com>
2022-01-27 07:23:25 +0900

    비동기

--- 3 JavaScript/DOM이벤트/ChangeAndInputEvents/app.js
+++ 3 JavaScript/DOM이벤트/ChangeAndInputEvents/app.js
@@ -1,10 +1,13 @@
-const input = document.querySelector('input');
-const h1 = document.querySelector('h1');
+const input = document.querySelector("input");
+const h1 = document.querySelector("h1");
 
 // input.addEventListener('change', function (e) {
 //     console.log("CASKDJASKJHD")
 // })
+console.log("Welcome, ".length);
 
-input.addEventListener('input', function (e) {
-    h1.innerText = input.value;
-})
+input.addEventListener("input", function (e) {
+  console.log(e);
+  h1.innerText = input.value;
+  console.log(input.value.length);
+});
