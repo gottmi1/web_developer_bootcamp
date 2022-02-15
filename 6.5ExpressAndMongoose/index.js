75f0a830026813f110a5789bd17a1c9b4956c3a2
gottmi1 <91591041+gottmi1@users.noreply.github.com>
2022-02-15 23:20:07 +0900

    ExpressAndMongoose

--- 6.5ExpressAndMongoose/index.js
+++ 6.5ExpressAndMongoose/index.js
@@ -2,23 +2,63 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
-mongoose
-  .connect("mongodb://localhost:27017/test", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+const methodOverride = require("method-override");
+
+const Product = require("./models/product");
+
+main()
   .then(() => {
-    console.log("coc");
+    console.log("CONNECT OPEN");
   })
   .catch((err) => {
-    console.log("tgmnjdfgjdf");
+    console.log("NO CONNECT");
+    console.log(err);
   });
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/farmStand");
+}
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
-app.get("/dog", (req, res) => {
-  res.send("wood!asd");
+app.get("/products", async (req, res) => {
+  const products = await Product.find({});
+  res.render("products/index", { products });
+});
+
+app.get("/products/new", (req, res) => {
+  res.render("products/new");
+});
+
+app.post("/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  res.redirect(`/products/${newProduct._id}`);
+});
+
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  // console.log(product);
+  res.render("products/show", { product });
+});
+
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product });
+});
+
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+  });
+  console.log(req.body);
+  res.redirect(`/products/${product._id}`);
 });
 
 app.listen(3000, () => {
