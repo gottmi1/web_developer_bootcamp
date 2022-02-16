235047a3e583f8fca1b52f4218bbf49b7c1079c7
gottmi1 <91591041+gottmi1@users.noreply.github.com>
2022-02-16 10:40:45 +0900

    CRUD with ExpressAndMongoose

---
+++ 6.5ExpressAndMongoose/README.md
@@ -0,0 +1,3 @@
+Express와 Mongoose를 사용해 기본적인 crud기능을 구현해 보았습니다.
+
+업데이트와 삭제를 구현하기 위해 method-override패키지를 사용하였습니다.
--- 6.5ExpressAndMongoose/index.js
+++ 6.5ExpressAndMongoose/index.js
@@ -24,41 +24,60 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 
+const categories = ["fruit", "vegetable", "dairy"];
+
 app.get("/products", async (req, res) => {
-  const products = await Product.find({});
-  res.render("products/index", { products });
-});
+  const { category } = req.query;
+  if (category) {
+    const products = await Product.find({ category: category });
+    res.render("products/index", { products, category });
+  } else {
+    const products = await Product.find({});
+    res.render("products/index", { products, category: "All" }); // 디폴트 값을 설정해줘서 카테고리가 없을 때 all이 보이게 한다
+  }
+}); // 카테고리가 있는 경우, 해당 카테고리를 포함한 것만 출력, else(카테고리가 없을 때) 모두 출력
 
 app.get("/products/new", (req, res) => {
-  res.render("products/new");
+  res.render("products/new", { categories });
 });
 
+// Create
 app.post("/products", async (req, res) => {
   const newProduct = new Product(req.body);
   await newProduct.save();
+  // console.log(newProduct);
   res.redirect(`/products/${newProduct._id}`);
 });
 
 app.get("/products/:id", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
-  // console.log(product);
   res.render("products/show", { product });
 });
 
+//Edit form
 app.get("/products/:id/edit", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findById(id);
-  res.render("products/edit", { product });
+  res.render("products/edit", { product, categories });
 });
 
+// Update
 app.put("/products/:id", async (req, res) => {
   const { id } = req.params;
   const product = await Product.findByIdAndUpdate(id, req.body, {
     runValidators: true,
   });
-  console.log(req.body);
+  // console.log(req.body);
   res.redirect(`/products/${product._id}`);
+});
+
+// Delete
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const deletedProduct = await Product.findByIdAndDelete(id);
+  // console.log(req.params);
+  res.redirect("/products");
 });
 
 app.listen(3000, () => {
