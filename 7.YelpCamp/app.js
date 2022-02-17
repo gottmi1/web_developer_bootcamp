13348cde361cd93108b69c2b197882c780df5751
gottmi1 <91591041+gottmi1@users.noreply.github.com>
2022-02-17 12:52:58 +0900

    CRUD

--- 7.YelpCamp/app.js
+++ 7.YelpCamp/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const methodOverride = require("method-override");
 const Campground = require("./models/Campground");
+const { urlencoded } = require("express");
 
 main()
   .then(() => {
@@ -22,6 +24,9 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 // 기본 폴더 경로를 views/ 로 만들기 위한 작업
 
+app.use(urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -31,12 +36,43 @@ app.get("/campgrounds", async (req, res) => {
   res.render("campgrounds/index", { campgrounds });
 });
 
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+// campgrounds/:id 아래에 두면 new를 id로 인식하기 때문에 위에둬야 함.
+
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
 app.get("/campgrounds/:id", async (req, res) => {
   const { id } = req.params;
   const campground = await Campground.findById(id);
   res.render("campgrounds/show", { campground });
 });
 // 클릭한 해당 링크의 id를 받아옴
+
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findById(id);
+  res.render("campgrounds/edit", { campground });
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground, // 이 부분 중괄호로 감싸야 작동함
+  });
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect("/campgrounds");
+});
 
 app.listen(3000, () => {
   console.log("port 3000에서 Serving 중 . . .");
