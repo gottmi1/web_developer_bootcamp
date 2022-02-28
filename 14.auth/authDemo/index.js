b878e4a82ef68f105acaa05e4b8d898d1cc54e5c
gottmi1 <91591041+gottmi1@users.noreply.github.com>
2022-02-28 20:07:02 +0900

    인증 및 권한부여

--- 14.auth/authDemo/index.js
+++ 14.auth/authDemo/index.js
@@ -3,6 +3,7 @@ const app = express();
 const User = require("./models/user");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
+const session = require("express-session");
 
 main().catch((err) => console.log(err));
 
@@ -10,7 +11,16 @@ async function main() {
   await mongoose.connect("mongodb://localhost:27017/authDemo");
 }
 
-app.use(express.urlencoded({ extended: true })); // req.bdoy에 접근하기 위해 필요. 패스
+app.use(express.urlencoded({ extended: true })); // req.bdoy에 접근하기 위해 필요.
+app.use(session({ secret: "yoyo" }));
+
+const requireLogin = (req, res, next) => {
+  // req.session에 user_id가 있는지 확인하는 미들웨어 함수
+  if (!req.session.user_id) {
+    return res.redirect("/login");
+  }
+  next();
+};
 
 app.set("view engine", "ejs");
 app.set("views", "views");
@@ -25,15 +35,17 @@ app.get("/register", (req, res, next) => {
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  // 그냥 디스쳐럭터링
-  const hash = await bcrypt.hash(password, 15);
-  const user = new User({
-    username, // 이건 그대로 저장
-    password: hash,
-  });
+  // const hash = await bcrypt.hash(password, 15);
+  // const user = new User({
+  //   username, // 이건 그대로 저장
+  //   password: hash,
+  // });
+  const user = new User({ username, password });
   await user.save();
   // password를 salt하고, db에 저장할 땐 salt한 값이 들어가도록 해줌
-  res.redirect("/");
+  req.session.user_id = user._id;
+  // 회원가입을 하면 즉시 로그인상태이기 때문에 여기도 추가해준다.
+  res.redirect("/secret");
 });
 
 app.get("/login", (req, res) => {
@@ -42,17 +54,31 @@ app.get("/login", (req, res) => {
 
 app.post("/login", async (req, res) => {
   const { username, password } = req.body;
-  const user = await User.findOne({ username });
-  const validPassword = await bcrypt.compare(password, user.password);
-  if (validPassword) {
-    res.send("로그인 성공");
+  // const user = await User.findOne({ username });
+  // const validPassword = await bcrypt.compare(password, user.password);
+  const foundUser = await User.findAndValidate(username, password);
+  if (foundUser) {
+    req.session.user_id = foundUser._id;
+    // session에 id를 추가하여 로그인 상태라는 것을 검증한다
+    res.redirect("/secret");
   } else {
-    res.send("아이디나 비밀번호가 틀렸어");
+    res.redirect("/login");
   }
 });
 
-app.get("/secret", (req, res) => {
-  res.send("로그인 하지 않으면 볼수 없다");
+app.post("/logout", (req, res) => {
+  req.session.user_id = null; //인증을 위해 필요한 최소한의 값 = 사용자 id추적
+  // req.session.destroy() 한 프로퍼티만 제거하지 않고 모든것을 제거하는 메서드
+  res.redirect("/login");
+});
+// 로그아웃 개념 = 간단하게 req.session에서 user_id를 지워주는 것
+
+app.get("/secret", requireLogin, (req, res) => {
+  // if (!req.session.user_id) {
+  //   return res.redirect("/login");
+  //   // user_id 값이 없다면 login화면으로 돌려보낸다
+  // } 미들웨어 함수를 정의했으므로 미들웨어 함수에서 처리한다
+  res.render("secret");
 });
 
 app.listen(3000, () => {
--- 14.auth/authDemo/models/user.js
+++ 14.auth/authDemo/models/user.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
@@ -9,6 +10,18 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, "페스워드를 입력해주세요"],
   },
+});
+
+// 미들웨어 추가
+userSchema.statics.findAndValidate = async function (username, password) {
+  const foundUser = await this.findOne({ username }); // 인자로 받은 username을 찾는다
+  const isValid = await bcrypt.compare(password, foundUser.password); // 인자로 받은 password와 foundUser의 password가 같은지 확인(compare은 true혹은 false를 반환)
+  return isValid ? foundUser : false; // isValid가 true라면 foundUser를 false로 한다
+};
+userSchema.pre("save", async function (next) {
+  if (!this.isModefied("password")) return next();
+  this.password = await bcrypt.hash(this.password, 12);
+  next();
 });
 
 module.exports = mongoose.model("User", userSchema);
